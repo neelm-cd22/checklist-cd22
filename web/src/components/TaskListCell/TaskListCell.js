f3f372d96ae7998e427579231250a3a7fd06a176
Nick <nickhit3802@gmail.com>
2022-08-04 19:54:02 -0700

    User can edit tasks but there is bug

--- web/src/components/TaskListCell/TaskListCell.js
+++ web/src/components/TaskListCell/TaskListCell.js
@@ -1,6 +1,5 @@
 import { Form, Submit, TextField } from '@redwoodjs/forms'
 import { useMutation } from '@redwoodjs/web'
-import { useState } from 'react';
 
 export const QUERY = gql`
 query FindTaskListQuery($id: Int!) {
@@ -30,16 +29,6 @@ const UPDATE_TASK = gql`
   }
 `
 
-// const UPDATE_TODO_MUTATION = gql`
-//   mutation UpdateTodoMutation($id: Int!, $input: UpdateTodoInput!) {
-//     updateTodo(id: $id, input: $input) {
-//       id
-//       body
-//       status
-//     }
-//   }
-// `
-
 export const Loading = () => <div>Loading...</div>
 
 export const Empty = () => <div>Empty</div>
@@ -63,12 +52,13 @@ export const Success = ({ taskList }) => {
     })
   }
 
-  const onUpdate = (id) => {
+  const onUpdate = (id, body) => {
+    console.log(body)
     updateTask({
       variables: {
         taskId: id,
         input: {
-          body: "Test",
+          body: body,
         }
       },
       refetchQueries: ['FindTaskListQuery']
@@ -84,20 +74,12 @@ export const Success = ({ taskList }) => {
             type="text"
             className="body"
             defaultValue={taskList.body}
-            onBlur={() => {
-              console.log("Triggered because this input lost focus"); // Debug
-              onUpdate(taskList.id) // Need to find way to send data to onUpdate function when focus is lost
+            onBlur={(e) => {
+              let newBody = e.target.value
+              onUpdate(taskList.id, newBody)
             }}
           />
         </label>
-
-        <Form className="temp" onBlur={(event) => { // This form is an experiment for sending data to the onUpdate function
-          console.log(event)
-        }}>
-          <TextField name="body" placeholder="ENTER HERE" />
-          <Submit>+</Submit>
-        </Form>
-
         <button
           type="button"
           className="inline-flex items-center mt-2 py-2 px-3 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-red-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
